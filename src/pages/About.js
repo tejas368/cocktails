3a69338ff8293cc43f45fba5e2d31aacdfa569a1
Tejaswi Anand <tejasanand1997@gmail.com>
2021-02-02 12:30:26 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className="section-title">about us</h1>
+      <p style={{color:'white'}}>
+        ggggggggggggggggggggggggggjjjjjjjjjjjjjjjjjjjggggggggggggggkkkkkkkkkkkkkkkuuuuuuuuuuuuuu
+        ffffffffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbjjjjjjjjjjjjjjjjjjyyyyyyyyyyyyyyyyfff
+        fffffffffffffffffjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkiiiiiiiiiiiiiiiiiyyyyyyyyyyyyyrrrrrrrrrr
+        rrrrrrdd
+        dddddddddddddddduuuuubbbbbbbbbbbbbbbbkkkkkkkkkuuuuuuuuuuuuuuuuuuufiiiiiiiiiiiiii
+        iiiiiii
+        iiiiiiiiiiiddddddddddddddddddddddddddddddddddddddddeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrrrrtttttttttt
+        ttttttttttttttttttwwwwwwwwwwwwwwwwwwwweeeeeeeeeeeeeeeeeqqqqqqqqqqqqqqqqqqqqqqqqyyyyyyyyyyyyyyyyyyyyyyyyypppppppp
+        ppppppppppppppppppppppppppppppnjjjjjjjjjjjjjjjjiiiiiiiiiiiiiii
+        iyfffffffffffffffffffffffyyyyyyyyyyy
+      </p>
+    </section>
+  )
+}
+
+export default About
